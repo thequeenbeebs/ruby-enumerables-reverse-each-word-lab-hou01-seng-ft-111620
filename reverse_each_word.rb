9c17fdb34e35079f1a98abb8786841110b20897b
Blaire Baker <bakerblaire@gmail.com>
2020-10-26 21:11:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,6 @@
 def reverse_each_word (string)
   string = string.split(" ")
-  reversed = []
   string.collect do |word|
     word = word.reverse
-    reversed.push(word)
   end
-  reversed.join(" ")
 end
